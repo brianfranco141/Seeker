1962ce6daa1876e5755b91e577573104d2ea215b
floresdanny01 <42793001+floresdanny01@users.noreply.github.com>
2022-04-05 11:55:00 -0700

    Update ResumeHandler.cpp

--- Domain/Resume/ResumeHandler.cpp
+++ Domain/Resume/ResumeHandler.cpp
@@ -1,16 +1,17 @@
 #include "ResumeHandler.hpp"
 
-
-std::string Logical_View::Domain::Resume::ResumeHandler::uploadResume(Resume resume) {
-	std::cout << "Successfully Uploaded \n "; 
-	return "test";
+void Logical_View::Domain::Resume::ResumeHandler::uploadResume(std::string name, std::string workExperience, std::string professionalSkills, std::string interests, std::string volunteerWork, std::string education) {
+	std::cout << "Resume Review Response \n";
 }
 
-void Logical_View::Domain::Resume::ResumeHandler::reviewResume(Resume resume) {
-	std::cout << " Insert Resume Review here \n";
+void Logical_View::Domain::Resume::ResumeHandler::reviewResume(std::string name, std::string workExperience, std::string education, std::string professionalSkill, std::string interests, std::string volunteerWork) {
+	std::cout << "Resume Review Response \n";
 }
 
-std::string Logical_View::Domain::Resume::ResumeHandler::paymentInfoSubmitted(payment paymentDetails) {
+std::string Logical_View::Domain::Resume::ResumeHandler::paymentInfoSubmitted(std::string creditCardNumber, std::string cardHolderName, std::string csv, std::string amount) {
 	std::cout << "Payment Successful \n";
-  return "success";
+}
+
+void Logical_View::Domain::Resume::ResumeHandler::endSession() {
+	std::cout << "You Have Been Logged Out \n";
 }
